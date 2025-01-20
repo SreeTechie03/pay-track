7ac3832dbaea4151d3fc97c01f6c9e8b3e81efe0
P Shyamanth Kumar <shyamanthkumar2004@gmail.com>
2025-01-20 17:15:40 +0530

    modification on left sidebar and mobile version

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { IBM_Plex_Serif } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
+import { Sidebar } from "lucide-react";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
-
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
   weight: ["400", "700"],
@@ -27,11 +29,20 @@ export default function RootLayout({
 }:Readonly< {
   children: React.ReactNode;
 }>) {
+  const loggedIn={firstName:'Adrian', lastName:'JSM'};
   return (
-    <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
-        {children}
-      </body>
+    <html lang="en"><body>
+      <main className="flex h-screen w-full font-inter"><Sidebar user={loggedIn}/>
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
+      </main></body>
     </html>
   );
 }
